39c82d5ddd7c45e8719797e3ee82b1c8e6df9565
emilisaksson <emil@studiomollan.com>
2021-04-08 11:40:57 +0200

    init spring

---
+++ inl2/src/main/java/inl2/inl2/Inl2Application.java
@@ -0,0 +1,13 @@
+package inl2.inl2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Inl2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Inl2Application.class, args);
+	}
+
+}
---
+++ inl2/src/test/java/inl2/inl2/Inl2ApplicationTests.java
@@ -0,0 +1,13 @@
+package inl2.inl2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Inl2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
